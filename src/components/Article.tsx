3c678173ee7128f1d643d934c06d4f456a87d397
Kenny <kenny16372@gmail.com>
2024-02-11 18:42:07 +0100

    fix: Fix carousel; Add fallback to scrolling

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -6,14 +6,20 @@ import Carousel from './Carousel';
 export default function Article(props: { query: string; variables: object; data: ArticleQuery }) {
   const { data } = useTina(props);
 
-  return (<>
-    <div className="article">
-      <div className="text-center">{new Date(data.article.createdAt).toLocaleDateString('de')}</div>
-      <h2 id={data.article.slug ?? ''}><a href={data.article.slug ? '#' + data.article.slug : ''} className="no-underline">{data.article.title}</a></h2>
-      <TinaMarkdown content={data?.article?.body ?? { type: 'root', children: [] }} components={{ Carousel }} />
+  return (
+    <article className="article">
+      <header>
+        <div className="text-center">{new Date(data.article.createdAt).toLocaleDateString('de')}</div>
+        <h2 id={data.article.slug ?? ''}>
+          <a href={data.article.slug ? '#' + data.article.slug : ''} className="no-underline">
+            {data.article.title}
+          </a>
+        </h2>
+      </header>
+      <main>
+        <TinaMarkdown content={data?.article?.body ?? { type: 'root', children: [] }} components={{ Carousel }} />
+      </main>
       <hr />
-      <Carousel images={["/uploads/2022_generationenturnier/gewinner.jpg", "/uploads/2022_generationenturnier/ergebnisse.jpg"]} />
-    </div>
-  </>
+    </article>
   );
 }
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,11 +1,29 @@
 export default function Carousel(props: { images?: string[] }) {
   return (
-    <div className="carousel" data-flickity='{"autoPlay": 4000}'>
-      {props.images?.map((image, index) => (
-        <div className="carousel-cell" key={index}>
-          <img src={image} alt={`Image ${index}`} />
-        </div>
-      ))}
-    </div>
+    <>
+      <div
+        className="carousel flex flex-row snap-x overflow-y-auto bg-primary-content"
+        data-flickity='{"imagesLoaded": true, "autoPlay": 4000, "fullscreen": true}'
+      >
+        {props.images?.map((image, index) => (
+          <img
+            className="block !rounded-none snap-start snap-normal max-w-[95%] !me-2 lg:max-h-[80vh]"
+            src={image}
+            alt={`Image ${index}`}
+            key={index}
+          />
+        ))}
+      </div>
+      <style>
+        {`.carousel.flickity-enabled {
+            display: block;
+          }
+
+          .carousel.flickity-enabled img {
+            max-width: 100%;
+          }
+        `}
+      </style>
+    </>
   );
 }
